c4dc462226678f4b8f4daf7992a885eadc92e2c1
TAHANG OPARA YVAN DIDIER <yvanlandry444@gmail.com>
2024-12-25 14:13:48 +0100

    food display issue solved

--- frontend/src/components/foodDisplay/FoodDisplay.jsx
+++ frontend/src/components/foodDisplay/FoodDisplay.jsx
@@ -1,38 +1,32 @@
-import React, { useContext } from 'react'
-import './foodDisplay.css'
-import { StoreContext } from '../../context/StoreContext'
-import FoodItem from '../foodItem/FoodItem'
+import React, { useContext } from 'react'; 
+import './foodDisplay.css'; 
+import { StoreContext } from '../../context/StoreContext'; 
+import FoodItem from '../foodItem/FoodItem'; 
 
+export default function FoodDisplay({ category }) { 
+    const { food_list } = useContext(StoreContext); 
 
-export default function FoodDisplay({category}) {
-
-    const  {food_list} = useContext(StoreContext)
-
-  return (
-    <div className='food-display' id='food-display'>
-        <h2>To deashes near you </h2>
-      
-        <div className="food-display-list">
-            {food_list.map((item,index) => {
-              if(category==='All'|| category===item.category){
-                console.log(category,item.category)
-                return ( 
-                
-                    <FoodItem
-                    key={index} 
-                    id={item._id} 
-                    name={item.name} 
-                    description={item.description}
-                    price={item.price} 
-                    image={item.image}
-                     />
-            )
-              }
-                
-            })
-                }
-        </div>
-      
-    </div>
-  )
+    return ( 
+        <div className='food-display' id='food-display'> 
+            <h2>To dishes near you</h2> 
+            <div className="food-display-list"> 
+                {food_list.map((item, index) => { 
+                    if (category === 'All' || category === item.category) { 
+                        console.log(category, item.category); 
+                        return ( 
+                            <FoodItem 
+                                key={index} 
+                                id={item._id} 
+                                name={item.name} 
+                                description={item.description} 
+                                price={item.price} 
+                                image={item.image} 
+                            /> 
+                        ); 
+                    } 
+                    return null; // Return null for items that don't match 
+                })} 
+            </div> 
+        </div> 
+    ); 
 }

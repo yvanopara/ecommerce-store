832e92792022aef69b19812b206125885bb198b3
TAHANG OPARA YVAN DIDIER <yvanlandry444@gmail.com>
2024-12-30 03:45:18 +0100

    lol

--- frontend/src/components/exploreMenu/ExploreMenu.js
+++ frontend/src/components/exploreMenu/ExploreMenu.js
@@ -6,8 +6,8 @@ import {menu_list} from '../../assets/assets'
 export default function ExploreMenu({category, setCategory}) {
   return (
     <div className='explore-menu' id='explore-menu'>
-      <h1>Explore our menu</h1>
-      <p style={{fontSize:'20px', color:'black',paddingLeft:'0'}}>Welcome to Restaurant Name, where we dedicated to providing an unforgettable dining experience...</p>
+      <h1>Explorez notre menu</h1>
+      {/* <p style={{fontSize:'20px', color:'black',paddingLeft:'0'}}>Welcome to Restaurant Name, where we dedicated to providing an unforgettable dining experience...</p> */}
 
       <div className='explore-menu-list'>
        {menu_list.map((item,index)=>{
@@ -15,7 +15,7 @@ export default function ExploreMenu({category, setCategory}) {
             <div onClick={() =>setCategory(prev=>prev===item.menu_name?'':item.menu_name)} key={index} className='explore-menu-list-item'>
               {/* setCategory(prev => ...);  prev is a parameter that represents the current value of the category state at the time of the update.  */}
                 <img className={category===item.menu_name?'active':''} src={item.menu_image} alt=''/>
-                <p>{item.menu_name ||'empty'}</p>
+                <p >{item.menu_name ||'empty'}</p>
             </div>
 
         )
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -5,9 +5,10 @@ export default function Header() {
   return (
     <div className='header'>
         <div className='header-contents '>
+          
             {/* <h2>Order your<br/> favorite food here</h2>
             <p>Welcome to Restaurant Name, where we dedicated to providing an unforgettable dining experience for each and every guest! Whether you’re visiting us for the first time or you’re one of our cherished regulars, we want to extend a warm and heartfelt welcome to you.</p> */}
-            <button>View menu</button>
+            {/* <button>View menu</button> */}
         </div>
       
     </div>

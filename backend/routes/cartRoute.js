7f502a82a25ca9067e254566b0660a1744676305
TAHANG OPARA YVAN DIDIER <yvanlandry444@gmail.com>
2025-01-13 18:28:48 +0100

    test

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -6,4 +6,4 @@ const cartRouter = express.Router();
 cartRouter.post('/add',authMiddleware,addToCart)
 cartRouter.post('/remove',authMiddleware,removeFromCart)
 cartRouter.post('/get',authMiddleware,getCart)
-export default cartRouter;
+export default cartRouter; 
--- backend/server.js
+++ backend/server.js
@@ -5,54 +5,39 @@ import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoot.js";
-import cartRouter from "./routes/carteRoute.js";
 import orderRouter from "./routes/orderRoute.js";
-import bodyParser from 'body-parser';
-
-
 import twilioRouter from "./routes/twilioRoute.js";
-
-// import orderRouter from "./routes/orderRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 // Load environment variables from .env file
 dotenv.config();
 
 // App configuration
 const app = express();
-const port = process.env.PORT || 5000; // Use PORT from .env or default to 5432
+const port = process.env.PORT || 5000; // Use PORT from .env or default to 5000
 
 // Middleware
 app.use(express.json()); // Parse incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
 app.use(cors());
 
-// Start server and connect to the database
+// Connect to the database
+connectDB();
 
-connectDB(); // Connect to the database
+// API endpoints
+app.use('/api/food', foodRouter);
+app.use("/images", express.static('uploads'));
+app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
+app.use('/api/twilio', twilioRouter);
 
-//api end point 
-app.use('/api/food',foodRouter)
-app.use("/images",express.static('uploads'))
-app.use('/api/user',userRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
-app.use('/api/twilio',twilioRouter)
-
-
-// Middleware to parse JSON requests
-app.use(bodyParser.json());
-
-
-
-
-
-
-app.listen(port, '0.0.0.0',() => {
-console.log(`Server started on http://localhost:${port}`);
- });
-
-
-// Routes
+// Default route
 app.get('/', (req, res) => {
     res.send('API IS WORKING');
 });
 
+// Start the server
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server started on http://localhost:${port}`);
+});

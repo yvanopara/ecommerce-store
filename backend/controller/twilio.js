002cf0932a2866107a5669ea404cfbf106b69fc5
TAHANG OPARA YVAN DIDIER <yvanlandry444@gmail.com>
2025-01-13 17:53:01 +0100

    twilio test3

--- backend/controller/twilio.js
+++ backend/controller/twilio.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 
 
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = "AC32c217d92414d19ed0facb149f105262";
+const authToken = "523dfc48499f6997ccf7b8053d3c220b";
 const client = twilio(accountSid, authToken);
 const sendTwilioMessage = async (req, res) => {  // Renamed the function
     const { message } = req.body;  // Extract message from the body

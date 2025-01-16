0300c09ad277fd21437eb00aa23a5712e9432040
yvanopara <136276693+yvanopara@users.noreply.github.com>
2025-01-16 13:06:20 +0100

    Update foodModel.js

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const foodSchema =  new mongoose.Schema({
     name: {type: String, required: true},
     description: {type: String, required: true},
-    price:{type: String, required: true},
+    price:{type: Number, required: true},
     image:{type: String, required: true},
     category: {type: String, required: true},
 
